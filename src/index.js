d98780e6daf5f4b2a07949ad83ff9d912874d540
joelcowie <joel.a.cowie@gmail.com>
2017-05-13 21:42:07 -0400

    implement search bar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyD0c-Y7XomSynTL86yPGHjjeANYLS2x-hk';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+  console.log(data);
+});
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
